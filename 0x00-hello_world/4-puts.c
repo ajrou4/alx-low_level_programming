72ae420e8c25e770487257a40687a8c3381332ed
ulv <36647431+ajrou4@users.noreply.github.com>
2023-02-17 01:35:35 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-
-int	main(void)
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints a string the standard output'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("Programming is like building a multiling puzzle \n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
