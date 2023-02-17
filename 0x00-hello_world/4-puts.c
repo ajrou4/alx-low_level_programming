84e6b05b658795fd49068d46fa4079ae902a825b
mohamed <mohamed1999ajro@gmail.com>
2023-02-16 16:22:30 -0800

    is my first programe in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int	main(void)
+{
+	puts("Programming is like building a multiling puzzle \n");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include<sdtio.h>
+
+int	main(void)
+{
+	printf("whith proper grammar,but the outcome is a piece of art,\n");
+	return 0;
+}
